d262419ccb04fecd1a51e137628a4b228eb12cad
Xuehua Chen <xuehuachen@Xuehuas-MacBook-Air.local>
2017-09-30 23:15:41 -0400

    test rlm: iteratively re-weighted least squares (IWLS) compared to lm

---
+++ test RLM.R
@@ -0,0 +1,7 @@
+library(MASS)
+data("anscombe")  # dataset that compare different cases where data are good or bad for linear regression (lm)
+plot(y3 ~ x3, data = anscombe)
+abline(lm(y3 ~ x3, data = anscombe),
+       col = "blue", lty = 2, lwd = 2)
+abline(rlm(y3 ~ x3, data = anscombe),
+       col = "red", lty = 1, lwd = 2)
